c0092e56f5bbba7f739cc7ca1886a7c795119a2f
Dhirendra1506 <kambledhirendra@gmail.com>
2022-11-22 19:20:41 +0530

    frist commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import MyFruits, { Mango, Apple } from './Fruits';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <Mango/>
+    <Apple/>
+    <MyFruits/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Fruits.js
@@ -0,0 +1,17 @@
+export function Apple() {
+    return (
+      <>Apple 🍎</>
+    );
+  }
+  export function Mango() {
+    return (
+      <>Mango 🥭</>
+    );
+  }
+
+  export default function MyFruits() {
+    return (
+      <>Apple 🍎 Mango 🥭</>
+    );
+  }
+
